b624f284be1906af15f7b0a239fc3f30b2d86d28
David Kelly <thedavidkelly@hotmail.com>
2016-06-02 17:38:03 +0100

    problem 22 added

---
+++ problem_22.hs
@@ -0,0 +1,21 @@
+-- Project Euler 22
+
+import Data.List
+import qualified Data.Map as Map
+import Data.List.Split
+
+cleanup :: String -> String
+cleanup = map (\x -> if x == '"' || x == ',' then ' ' else x)
+
+sumName :: String -> Int
+sumName xs = sum $ sum <$> map (`Map.lookup` vals) xs
+
+vals :: Map.Map Char Int
+vals = Map.fromList $ zip ['A'..'Z'] [1..]
+
+main :: IO ()
+main = do
+    names <- readFile "./p022_names.txt"
+    print $ sum $ 
+        map (\(x,y) -> x * sumName y) $ zip [1..] $ 
+            dropWhile (== "") $ sort . splitOn " " . cleanup $ names
