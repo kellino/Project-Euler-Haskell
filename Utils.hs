40ccf9f0363bb5f2034c73b36cdbd7c2c8864138
David Kelly <dkellino@gmail.com>
2018-11-30 15:33:14 +0000

    problem 21 done

--- Utils.hs
+++ Utils.hs
@@ -3,10 +3,13 @@
 module Utils
     ( primes
     , modexp
+    , primeFactors
+    , properDivisors
     )
 where
 
 import Data.Bits
+import Data.List
 
 minus :: Ord a => [a] -> [a] -> [a]
 minus []       ys       = ys
@@ -16,8 +19,8 @@ minus (x : xs) (y : ys) = case compare x y of
     EQ -> minus xs ys
     GT -> minus (x : xs) ys
 
-primes :: [Int]
-primes = sieve [2..]
+primes :: [Integer]
+primes = 2 : sieve [3,5..]
     where sieve [] = []
           sieve (x:xs) = x : sieve (xs `minus` [x*x, x*x+x..])
 
@@ -27,3 +30,16 @@ modexp _ 0 _ = 1
 modexp b e m = 
     let c = if testBit e 0 then b `mod` m else 1
      in (c * modexp (b*b `rem` m) (shiftR e 1) m) `mod` m
+
+squareRoot :: Integer -> Integer
+squareRoot = ceiling . sqrt . fromIntegral
+
+primeFactors :: Integer -> [Integer]
+primeFactors 1 = []
+primeFactors n = foldr (\p v -> if n `rem` p == 0 then p:v else v) [] (genericTake (squareRoot n) primes)
+
+properDivisors :: Integer -> [Integer]
+properDivisors n = 
+    let start = foldr (\d v -> if n `rem` d == 0 then d:v else v) [] [2..squareRoot n]
+        end   = foldr (\d v -> if n `rem` d == 0 then (n `div` d):v else v) [] start
+     in 1 : start ++ reverse end
