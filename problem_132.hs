ae4b90bf510bbd31463e3bc69b68dab6e43db726
David Kelly <dkellino@gmail.com>
2018-11-30 09:08:04 +0000

    moved some code to Utils

--- problem_132.hs
+++ problem_132.hs
@@ -1,28 +1,6 @@
 module Main where
 
-import Data.Bits
-
-minus :: Ord a => [a] -> [a] -> [a]
-minus [] ys = ys
-minus xs [] = xs
-minus (x:xs) (y:ys) = 
-    case compare x y of
-        LT -> x : minus xs (y:ys)
-        EQ -> minus xs ys
-        GT -> minus (x:xs) ys
-
-primes :: [Int]
-primes = sieve [2..]
-    where sieve [] = []
-          sieve (x:xs) = x : sieve (xs `minus` [x*x, x*x+x..])
-
-
-modexp :: (Num t, Bits t, Integral a) => a -> t -> a -> a
-modexp _ _ 1 = 0
-modexp _ 0 _ = 1
-modexp b e m = 
-    let c = if testBit e 0 then b `mod` m else 1
-     in (c * modexp (b^2 `mod` m) (shiftR e 1) m) `mod` m
+import Utils
 
 euler :: Int -> [Int]
 euler k = foldr (\p v -> if modexp 10 k (9*p) == 1 then p:v else v) [] primes
